ce3ec74b11682c5576f550d1209435f75be04f4b
knhaila <nhailakaoutar@gmail.com>
2021-12-30 15:18:45 +0100

    intial project

---
+++ src/main/scala/Node.scala
@@ -0,0 +1,20 @@
+package upmc.akka.leader
+
+import akka.actor._
+
+case class Start ()
+
+class Node (val id:Int) extends Actor {
+
+     // Les differents acteurs du systeme
+     val displayActor = context.actorOf(Props[DisplayActor], name = "displayActor")
+
+     def receive = {
+
+          // Initialisation
+          case Start => {
+               displayActor ! Message ("Node " + this.id + " is created")        
+          }
+
+     }
+}
